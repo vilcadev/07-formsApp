f83bf2e63b9334f12c6ebad0a21e78cb2c274fea
cesar <vcesar2021@gmail.com>
2023-11-18 17:42:48 -0500

    fin seccion 17, forms app 85%

--- src/app/reactive/pages/basic-page/basic-page.component.ts
+++ src/app/reactive/pages/basic-page/basic-page.component.ts
@@ -1,12 +1,18 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+
+const rtx5090 ={
+  name: 'RTX 5090',
+  price: '2500',
+  inStorage: '6',
+}
 
 @Component({
   templateUrl: './basic-page.component.html',
   styles: [
   ]
 })
-export class BasicPageComponent {
+export class BasicPageComponent implements OnInit{
 
    // * 1ra era Forma
   // public myForm: FormGroup = new FormGroup({
@@ -25,8 +31,43 @@ export class BasicPageComponent {
 
   constructor(private fb: FormBuilder){}
 
+  ngOnInit(): void {
+    // this.myForm.reset( rtx5090 );
+  }
+
+  // Método que nos sirve para saber si el campo en particular tiene algún error.
+  isValidField(field: string): boolean | null{
+    return this.myForm.controls[field].errors && this.myForm.controls[field].touched;
+  }
+
+  getFieldError(field: string): string | null{
+    if(! this.myForm.controls[field]) return null;
+
+    // || {}  -> Si esto es nulo entonces regresemos null para no seguir
+    const errors = this.myForm.controls[field].errors || {};
+
+      for (const key of Object.keys(errors)) {
+          switch( key ){
+            case 'required':
+              return 'Este campo es requerido';
+            case 'minlength':
+              return `Mínimo ${ errors['minlength'].requiredLength } carácteres`;
+          }
+    }
+    return 'null';
+  }
+
+
+
+
+
   onSave(): void{
-    if(this.myForm.invalid) return;
+    if(this.myForm.invalid){
+      this.myForm.markAllAsTouched();
+      return;
+    }
     console.log(this.myForm.value);
+
+    this.myForm.reset({ price:10, inStorage:0 });
   }
 }
--- src/app/reactive/pages/dynamic-page/dynamic-page.component.ts
+++ src/app/reactive/pages/dynamic-page/dynamic-page.component.ts
@@ -1,5 +1,5 @@
 import { Component } from '@angular/core';
-import { FormBuilder, FormGroup } from '@angular/forms';
+import { FormArray, FormBuilder, FormControl, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   templateUrl: './dynamic-page.component.html',
@@ -8,6 +8,91 @@ import { FormBuilder, FormGroup } from '@angular/forms';
 })
 export class DynamicPageComponent {
 
+  // ** 1era Forma
+  // public myForm2 = new FormGroup({
+  //   favoriteGames: new FormArray([])
+  // });
 
+   // ** 2da Forma
+  public myForm: FormGroup = this.fb.group({
+    name: ['', [Validators.required, Validators.minLength(3)]],
+    favoriteGames: this.fb.array([
+      ['Metal Gear', Validators.required],
+      ['Death Stranding', Validators.required],
+    ])
+  })
+
+  public newFavorite: FormControl = new FormControl('', [Validators.required]);
+
+  constructor( private fb: FormBuilder){}
+
+  get favoriteGames(){
+    return this.myForm.get('favoriteGames') as FormArray;
+  }
+
+  // Método que nos sirve para saber si el campo en particular tiene algún error.
+  isValidField(field: string): boolean | null{
+    return this.myForm.controls[field].errors
+    && this.myForm.controls[field].touched;
+  }
+
+  isValidFieldInArray(formArray: FormArray, index:number){
+    return formArray.controls[index].errors
+    && formArray.controls[index].touched;
+  }
+
+
+  getFieldError(field: string): string | null{
+    if(! this.myForm.controls[field]) return null;
+
+    // || {}  -> Si esto es nulo entonces regresemos null para no seguir
+    const errors = this.myForm.controls[field].errors || {};
+
+      for (const key of Object.keys(errors)) {
+          switch( key ){
+            case 'required':
+              return 'Este campo es requerido';
+            case 'minlength':
+              return `Mínimo ${ errors['minlength'].requiredLength } carácteres`;
+          }
+    }
+    return 'null';
+  }
+
+  onAddToFavorites(): void{
+    if(this.newFavorite.invalid) return;
+
+    const newGame = this.newFavorite.value;
+
+    // Esto se haría si no estuviéramos trabajando con el FormBuilder:
+    // this.favoriteGames.push( new FormControl(newGame, Validators.required));
+
+    // Trabajando con FormBuilder:
+    this.favoriteGames.push(
+      this.fb.control( newGame, Validators.required )
+    );
+
+    this.newFavorite.reset();
+
+
+
+  }
+
+  onDeleteFavorite(index: number): void{
+    this.favoriteGames.removeAt(index)
+  }
+
+
+
+  onSubmit(){
+    if(this.myForm.invalid){
+      this.myForm.markAllAsTouched();
+      return;
+    }
+
+    console.log(this.myForm.value);
+    (this.myForm.controls['favoriteGames'] as FormArray) = this.fb.array([]);
+    this.myForm.reset();
+  }
 
 }
